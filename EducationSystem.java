b5eee8daec8ca048a2fba6ab69b04ecc6f154ef7
Malenia <82274616+TheMalenia@users.noreply.github.com>
2024-05-31 21:15:04 +0330

    Update EducationSystem.java

--- EducationSystem.java
+++ EducationSystem.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+
 interface EducationalInstitution {
     String displayInfo();
 }
@@ -10,10 +11,24 @@ abstract class University implements EducationalInstitution {
     public ArrayList<Student> students;
 
     public boolean transferStudent(Student student, University university) {
+        if(students.contains(student) == true){
+            students.remove(student);
+            studentCount--;
+            student.setUniversity(university);
+            university.students.add(student);
+            university.studentCount++;
+            return true;
+        }
+        return false;
+    }
+    
+    University(String name, double tuitionFee) {
+        this.name = name;
+        this.studentCount = 0;
+        this.tuitionFee = tuitionFee;
+        this.students = new ArrayList<>();
     }
 
-    University(String name, double tuitionFee) {
-    }
 }
 
 class Student implements EducationalInstitution {
@@ -22,41 +37,94 @@ class Student implements EducationalInstitution {
     private double balance;
 
     public Student(String name, double balance, University university) {
+        if(balance >= university.tuitionFee){
+            this.name = name;
+            this.balance = balance - university.tuitionFee;
+            this.university = university;
+            university.students.add(this);
+            university.studentCount++;
+        }
+        else {
+            System.out.println("Not Enough Balance");
+        }
     }
 
     @Override
     public String displayInfo() {
+        return "Name: " + name + " | University: " + university.name + " | Balance: " + balance;
     }
 
     public String getName() {
+        return name;
     }
-
     public void setName(String name) {
+        this.name = name;
     }
 
     public University getUniversity() {
+        return university;
+    }
+    public void setUniversity(University university) {
+        this.university = university;
     }
 
     public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 }
 
 class SbuUniversity extends University {
     public SbuUniversity(String name, double tuitionFee) {
+        super(name, tuitionFee);
     }
 
     @Override
     public String displayInfo() {
+        return "SBU University:\nTuition Fee: " + String.format("%.1f", tuitionFee) + " | Student Count: " + studentCount;
     }
 }
 
 class SutUniversity extends University {
 
-    SutUniversity(String name, double tuitionFee) {
+    public SutUniversity(String name, double tuitionFee) {
+        super(name, tuitionFee);
     }
 
     @Override
     public String displayInfo() {
+        return "SUT University:\nTuition Fee: " + String.format("%.1f", tuitionFee) + " | Student Count: " + studentCount;
+    }
+}
+
+
+public class EducationSystem {
+    public static void main(String[] args){
+        // tests
+        SbuUniversity sbu = new SbuUniversity("SBU", 1000);
+        SutUniversity sut = new SutUniversity("SUT", 1200);
+        System.out.println(sbu.displayInfo());
+        System.out.println(sut.displayInfo());
+        Student sbuStudent = new Student("Hasan", 1500, sbu);
+        Student sutStudent1 = new Student("Shima", 1500, sut);
+        Student sutStudent2 = new Student("Setareh", 100, sut);
+        System.out.println("---------------------------");
+        System.out.println("Before transfer:");
+        System.out.println(sbu.displayInfo());
+        System.out.println(sut.displayInfo());
+        System.out.println(sbuStudent.displayInfo());
+        System.out.println(sutStudent1.displayInfo());
+        System.out.println("---------------------------");
+        boolean Done = sbu.transferStudent(sbuStudent, sut);
+        System.out.println("Transfer = " + Done);
+        System.out.println("---------------------------");
+        System.out.println("After transfer:");
+        System.out.println(sbu.displayInfo());
+        System.out.println(sut.displayInfo());
+        System.out.println(sbuStudent.displayInfo());
+        System.out.println(sutStudent1.displayInfo());
 
     }
 }
